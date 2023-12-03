b3eaa6cdcf0089fd901019352db39fdeee4fd0b8
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2023-12-03 10:43:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import cors from 'cors'
 export const app = express();
 
 // connecting to database
-connectDB(app.get("env") == "development" ? process.env.DB_URI_LOCAL : process.env.DB_URI_PROD)
+connectDB(app.get("env") == "development" ? "mongodb://localhost:27017/ghblog" : process.env.DB_URI_PROD)
 
 // imported routes || this is used in line 31 to line 32
 import userRouter from './routes/user/userRoutes.js'
